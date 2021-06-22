4eacbcd41b391900027828bacfe740e8cfe58ae5
zhougx <zhougx.com>
2021-06-22 20:18:51 +0800

    7

--- seven/0709/src/main/java/com/zhou/seven09/input/Insert100.java
+++ seven/0709/src/main/java/com/zhou/seven09/input/Insert100.java
@@ -1,44 +1,67 @@
 package com.zhou.seven09.input;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.pool.HikariPool;
+
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Insert100 {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        String url="jdbc:mysql://localhost:3306/baotao";
-        String username="root";
-        String password="";
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(url, username, password);
 
-        String sql="insert into user (user_id,username,password,nikename,createtime,updatetime) values ";
+    final int total=1000000;
+    final int ever=5000*2;
 
-        List<String> sqls=new ArrayList<>();
+    String url="jdbc:mysql://localhost:3306/baotao?rewriteBatchedStatements=true";
+    String username="root";
+    String password="";
 
-        StringBuffer sb;
-        for (int i = 0; i < 100; i++) {
-            sb=new StringBuffer();
-            for (int j = i*10000; j < (i+1)*10000; j++) {
-                String s = String.valueOf(j + 1);
-                long l = System.currentTimeMillis();
-                sb.append("('").append(s).append("','name").append(s).append("','password")
-                        .append(s).append("','nick").append(s).append("',").append(l).append(",")
-                        .append(l).append("),");
-            }
-            String sql2 = sql+sb.substring(0,sb.length()-1);
-            sqls.add(sql2);
+    AtomicInteger atomicInteger;
+
+    public void input(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setMaximumPoolSize(total/ever);
+        HikariPool hikariPool = new HikariPool(hikariConfig);
+
+        final String sql="insert into user (user_id,username,password,nikename,createtime,updatetime) values ";
+
+        atomicInteger=new AtomicInteger();
+
+        long start=System.currentTimeMillis();
+        for (int i = 0; i < total / ever; i++) {
+            final int ii=i;
+            StringBuffer stringBuffer = new StringBuffer(sql);
+            Runnable runnable = () -> {
+                for (int j = ii*ever; j < (ii + 1) * ever; j++) {
+                    long l=System.currentTimeMillis();
+                    stringBuffer.append("('").append(j+1).append("','name_").append(j+1).append("','password_")
+                            .append(j+1).append("','nick_").append(j+1).append("',").append(l).append(",")
+                            .append(l).append("),");
+                }
+                String sql_go = stringBuffer.substring(0, stringBuffer.length() - 1);
+                try {
+                    hikariPool.getConnection().prepareStatement(sql_go).executeUpdate();
+                    int n = atomicInteger.incrementAndGet();
+                    System.out.println("finish-->"+n);
+                    if(n==total/ever){
+                        long end=System.currentTimeMillis();
+                        System.out.println((end-start)/1000+" seconds");
+                        System.exit(0);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            };
+            runnable.run();
         }
-        long start=System.currentTimeMillis()/1000;
-        System.out.println("start");
-        for (String sql3 : sqls) {
-            System.out.println(sql3);
-            connection.prepareStatement(sql3).executeUpdate();
-        }
-        long end=System.currentTimeMillis()/1000;
-        System.out.println("end");
-        System.out.println("耗时："+(end-start)+"秒");//大概46秒
+    }
+
+    public static void main(String[] args) {
+        Insert100 insert100 = new Insert100();
+        insert100.input();
     }
 }
